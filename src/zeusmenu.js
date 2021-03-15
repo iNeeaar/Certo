0e0a7f481d088049048036e7d6b887228a95ae27
iNeeaar <lucwsleandro@gmail.com>
2021-03-15 18:18:35 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,38 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Zeus:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
